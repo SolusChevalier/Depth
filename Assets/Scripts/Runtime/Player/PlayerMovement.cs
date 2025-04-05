4ae030dee5aa09352c9edf8c3d72c124320a1676
Adam Cargill <Adam@levett.biz>
2025-04-05 16:38:35 +0200

    testWall

--- Assets/Scripts/Runtime/Player/PlayerMovement.cs
+++ Assets/Scripts/Runtime/Player/PlayerMovement.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    #region FIELDS
+    public float swimSpeed = 5f;
+    public float swimAcceleration = 10f;
+    public float swimDeceleration = 10f;
+    public float maxSwimSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float CurrentPitch = 0f;
+    //public float maxPitch = 45f;
+    //public float minPitch = -45f;
+    public float pitchSpeed = 1f;
+
+    public PlayerManager playerManager;
+    
+
+    #endregion FIELDS
+
+    #region UNITY METHODS
+
+    #endregion UNITY METHODS
+
+
+    #region METHODS
+
+
+
+
+    #endregion METHODS
 }
