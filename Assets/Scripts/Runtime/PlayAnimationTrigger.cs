0fb92a83d4a413a24597d9e5c7098c10d1d50826
Dxrk-Reality <oliverm3364@gmail.com>
2025-04-08 19:25:16 +0200

    Eye animation

---
+++ Assets/Scripts/Runtime/PlayAnimationTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayAnimationTrigger : MonoBehaviour
+{
+    public Animator targetAnimator;
+    public string triggerName = "Play";
+
+    private bool hasPlayed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasPlayed && other.CompareTag("Player"))
+        {
+            targetAnimator.SetTrigger(triggerName);
+            hasPlayed = true;
+        }
+    }
+}
